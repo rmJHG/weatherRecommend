b54b5f3017fd649b4ebddbc0b47b7ed97eef3b2c
JangHanGyu <jhg990508@gmail.com>
2023-04-05 21:15:52 +0900

    20230405
    아이콘, 추천버튼 if문 수정

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ const API_KEY = "b0cb44432057bee080d3e3db2ed3a68d";
 const MAPAPI_KEY = "c19a0181393183574e358c5095a76439";
 let lat;
 let lon;
-let recommendNowTemp;
+
 function removeAllChild(div) {
   while (div.hasChildNodes()) {
     div.removeChild(div.firstChild);
@@ -55,8 +55,8 @@ function fetchBtn(name) {
 }
 
 //날씨별 아이콘 변경 및 날씨별 멘트 변경
-function weatherIcon(weather_data) {
-  // console.log(weather_data);
+function weatherIcon(weather_data, recommendNowTemp) {
+  // console.log(weather_data, recommendNowTemp);
   if (weather_data !== "Clear") {
     removeIcon("fa-sun");
   } else if (weather_data !== "Clouds") {
@@ -67,8 +67,8 @@ function weatherIcon(weather_data) {
     removeIcon("fa-cloud-rain");
   } else if (weather_data !== "Snow") {
     removeIcon("fa-snowflake");
-  }else if (weather_data === "Rain") {
-    addIcon("fa-Rain");
+  } else if (weather_data === "Rain") {
+    addIcon("fa-umbrella");
   }
   if (weather_data === "Clear") {
     addIcon("fa-sun");
@@ -80,42 +80,49 @@ function weatherIcon(weather_data) {
     addIcon("fa-cloud-rain");
   } else if (weather_data === "Snow") {
     addIcon("fa-snowflake");
-  }else if (weather_data === "Rain") {
-    addIcon("fa-rain");
+  } else if (weather_data === "Rain") {
+    addIcon("fa-umbrella");
   }
   let seasonOfMons;
-  if (2 < mons < 6) {
-    seasonOfMons = "봄";
-  } else if (8 < mons < 12) {
+  if (11 < mons) {
+    seasonOfMons = "겨울";
+  } else if (9 <= mons) {
     seasonOfMons = "가을";
+  } else if (6 <= mons) {
+    seasonOfMons = "여름";
+  } else if (3 <= mons) {
+    seasonOfMons = "봄";
+  } else if (mons < 3) {
+    seasonOfMons = "겨울";
   }
-  if (weather_data === "Cloudrain") {
+
+  if (weather_data === "Cloudrain" || weather_data === "Rain") {
     fetchPage("season/rain");
-    fetchBtn("season/rainBtn");
-  } else if (recommendNowTemp < 4) {
-    fetchPage("season/winter");
-    fetchBtn("seasonBtn/winterBtn");
-  } else if (4 <= recommendNowTemp < 8) {
-    fetchPage("season/48");
-    fetchBtn("seasonBtn/48Btn");
-  } else if (8 <= recommendNowTemp < 15 && weather_data === "Clouds") {
-    fetchPage("season/815Clouds");
-    fetchBtn("seasonBtn/815Btn");
-  } else if (8 <= recommendNowTemp < 15 && weather_data === "Wind") {
-    fetchPage("season/815Wind");
-    fetchBtn("seasonBtn/815Btn");
-  } else if (8 <= recommendNowTemp < 15 && weather_data === "Clear") {
-    fetchPage("season/815Clear");
-    fetchBtn("seasonBtn/815Btn");
-  } else if (15 <= recommendNowTemp < 23) {
-    fetchPage("season/1523");
-    fetchBtn("seasonBtn/1523Btn");
-  } else if (23 <= recommendNowTemp < 33) {
-    fetchPage("season/2333");
-    fetchBtn("seasonBtn/2333Btn");
-  } else if (33 <= recommendNowTemp) {
+    fetchBtn("seasonBtn/rainBtn");
+  } else if (34 < recommendNowTemp) {
     fetchPage("season/summer");
     fetchBtn("seasonBtn/summerBtn");
+  } else if (24 < recommendNowTemp) {
+    fetchPage("season/2333");
+    fetchBtn("seasonBtn/2333Btn");
+  } else if (16 < recommendNowTemp) {
+    fetchPage("season/1523");
+    fetchBtn("seasonBtn/1523Btn");
+  } else if (9 < recommendNowTemp && weather_data === "Clouds") {
+    fetchPage("season/815Clouds");
+    fetchBtn("seasonBtn/815Btn");
+  } else if (9 < recommendNowTemp && weather_data === "Wind") {
+    fetchPage("season/815Wind");
+    fetchBtn("seasonBtn/815Btn");
+  } else if (9 < recommendNowTemp && weather_data === "Clear") {
+    fetchPage("season/815Clear");
+    fetchBtn("seasonBtn/815Btn");
+  } else if (5 < recommendNowTemp) {
+    fetchPage("season/48");
+    fetchBtn("seasonBtn/48Btn");
+  } else if (recommendNowTemp <= 4) {
+    fetchPage("season/winter");
+    fetchBtn("seasonBtn/winterBtn");
   }
 }
 //좌표를 기준으로 현재 날씨정보 구하기
@@ -126,13 +133,12 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       nowTemp.innerText = data.main.temp + "℃";
-      recommendNowTemp = data.main.temp;
       ftTemp.innerText = data.main.feels_like + "℃";
       huTemp.innerText = data.main.humidity + "%";
       loc.innerText = `${data.sys.country}  ${data.name}`;
-      weatherIcon(data.weather[0].main);
+      weatherIcon(data.weather[0].main, data.main.temp);
     });
 }
 
@@ -154,10 +160,8 @@ function updateGeoInfo() {
 updateGeoInfo();
 setInterval(updateGeoInfo, 60000);
 
-//새로운 추천버튼 만들기
+//새로운 추천목록 만들기
 function hello(target) {
-  // console.log(target.target.value)
-  // console.log(target.path[0].value);
   delRecommendList();
   removeMarker();
   recommendSearch(target.target.value);
